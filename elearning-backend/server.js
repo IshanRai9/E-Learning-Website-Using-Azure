43d1de5a1cd11d915a47186b805e1326cd1fe5af
IshanRai9 <raiishan0911@gmail.com>
2025-03-29 20:58:49 +0530

    path changes

--- elearning-backend/server.js
+++ elearning-backend/server.js
@@ -5,10 +5,10 @@ const app = express();
 const PORT = 5000;
 
 // Serve React frontend
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../elearning-frontend/public")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../elearning-frontend/public", "index.html"));
 });
 
 // API routes
