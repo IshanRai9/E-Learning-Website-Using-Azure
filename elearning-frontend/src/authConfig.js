75f338a07cbfa599fb9899440fd48cd01e105adc
IshanRai9 <raiishan0911@gmail.com>
2025-03-31 23:47:29 +0530

    changed

--- elearning-frontend/src/authConfig.js
+++ elearning-frontend/src/authConfig.js
@@ -1,7 +1,7 @@
 const authConfig = {
   auth: {
     clientId: "310c4a66-3747-42a5-bbc5-a08c64103c9a",
-    authority: "https://elearningb2c.b2clogin.com/372a529e-01fe-41d3-af0e-629c487c0be9.onmicrosoft.com/B2C_1_SignUpSignIn",
+    authority: "https://login.microsoftonline.com/372a529e-01fe-41d3-af0e-629c487c0be9",
     redirectUri: "http://localhost:3000/auth/callback",
   },
 };
