f6c5bcea87968f5734456a62a68b1d350f684db0
IshanRai9 <raiishan0911@gmail.com>
2025-03-31 23:45:31 +0530

    auth changed

--- elearning-frontend/src/authConfig.js
+++ elearning-frontend/src/authConfig.js
@@ -1,5 +1,4 @@
-import { PublicClientApplication } from "@azure/msal-browser";
-const msalConfig = {
+const authConfig = {
   auth: {
     clientId: "310c4a66-3747-42a5-bbc5-a08c64103c9a",
     authority: "https://elearningb2c.b2clogin.com/372a529e-01fe-41d3-af0e-629c487c0be9.onmicrosoft.com/B2C_1_SignUpSignIn",
@@ -7,4 +6,4 @@ const msalConfig = {
   },
 };
 
-export const msalInstance = new PublicClientApplication(msalConfig);
+export default authConfig;
