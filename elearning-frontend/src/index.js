0350b386a7018344580936f86ad5f775c7227d07
IshanRai9 <raiishan0911@gmail.com>
2025-04-12 20:52:53 +0530

    rev

--- elearning-frontend/src/index.js
+++ elearning-frontend/src/index.js
@@ -1,24 +1,15 @@
-// index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AuthProvider } from "react-oidc-context";
+import { Amplify } from 'aws-amplify';
+import awsConfig from './authConfig';
 
-const cognitoAuthConfig = {
-  authority: "https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_p3Ax4AAf4",
-  client_id: "763kbchi8hhj014b65lafqj4ra",
-  redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
-  response_type: "code",
-  scope: "email openid phone",
-};
+// Configure Amplify
+Amplify.configure(awsConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// wrap the application with AuthProvider
 root.render(
   <React.StrictMode>
-    <AuthProvider {...cognitoAuthConfig}>
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
