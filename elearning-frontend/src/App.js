d2bf64e7368e300b7671c05d0fb4b47ecc3f0ad6
IshanRai9 <raiishan0911@gmail.com>
2025-04-12 20:48:47 +0530

    reverting

--- elearning-frontend/src/App.js
+++ elearning-frontend/src/App.js
@@ -1,43 +1,20 @@
-// App.js
-
-import { useAuth } from "react-oidc-context";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import Logout from "./Logout";
 
 function App() {
-  const auth = useAuth();
-
-  const signOutRedirect = () => {
-    const clientId = "763kbchi8hhj014b65lafqj4ra";
-    const logoutUri = "<logout uri>";
-    const cognitoDomain = "https://ap-south-1p3ax4aaf4.auth.ap-south-1.amazoncognito.com";
-    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
-  };
-
-  if (auth.isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (auth.error) {
-    return <div>Encountering error... {auth.error.message}</div>;
-  }
-
-  if (auth.isAuthenticated) {
-    return (
-      <div>
-        <pre> Hello: {auth.user?.profile.email} </pre>
-        <pre> ID Token: {auth.user?.id_token} </pre>
-        <pre> Access Token: {auth.user?.access_token} </pre>
-        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
-
-        <button onClick={() => auth.removeUser()}>Sign out</button>
-      </div>
-    );
-  }
-
   return (
-    <div>
-      <button onClick={() => auth.signinRedirect()}>Sign in</button>
-      <button onClick={() => signOutRedirect()}>Sign out</button>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
+    </Router>
   );
 }
 
