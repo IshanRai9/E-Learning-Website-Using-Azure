03383ebb8fdd3cb77ed436a1eeef24a196718f1f
IshanRai9 <raiishan0911@gmail.com>
2025-04-13 14:02:47 +0530

    login

--- elearning-frontend/src/authConfig.js
+++ elearning-frontend/src/authConfig.js
@@ -9,10 +9,10 @@ const awsConfig = {
     region: 'ap-south-1', // Replace with your region
     // Optional: OAuth configuration
     oauth: {
-      domain: 'ap-south-1p3ax4aaf4.auth.ap-south-1.amazoncognito.com', // Remove the https:// prefix
+      domain: 'auth.ap-south-1p3ax4aaf4.amazoncognito.com',
       scope: ['email', 'openid', 'profile'],
-      redirectSignIn: 'https://ap-south-1p3ax4aaf4.auth.ap-south-1.amazoncognito.com/login?client_id=763kbchi8hhj014b65lafqj4ra&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fd84l1y8p4kdic.cloudfront.net',
-      redirectSignOut: 'http://elearning-cclab.s3-website.ap-south-1.amazonaws.com/',
+      redirectSignIn: 'https://d84l1y8p4kdic.cloudfront.net',
+      redirectSignOut: 'https://d84l1y8p4kdic.cloudfront.net',
       responseType: 'code'
     }
   }
